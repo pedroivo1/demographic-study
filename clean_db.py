cb2ab4f85511fa0b400bdc7cb25a49bf60f19eff
pedroivo1 <pedroivoal1@gmail.com>
2025-12-28 23:05:52 -0300

    Chosed countrys

--- clean_db.py
+++ clean_db.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('data/população-mundial-WB.csv', skiprows=4)
 
-paises = ['Brazil', 'United States', 'Germany', 'Nigeria', 'Egypt', 'India', 'China']
+paises = [
+    'Nigeria', 
+    'Brazil', 
+    'United States', 
+    'Japan', 
+    'Korea, Rep.', 
+]
 df = df[df['Country Name'].isin(paises)].copy()
 anos = df.columns[4:-1]
 needed_columns = ['Country Name'] + list(anos)
@@ -10,4 +16,8 @@ needed_columns = ['Country Name'] + list(anos)
 df = df[needed_columns]
 df[anos] = df[anos].astype('Int64')
 
+df.rename(columns={'Country Name': 'Country'}, inplace=True)
+df['Country'] = df['Country'].replace('Egypt, Arab Rep.', 'Egypt')
+
 df.to_csv('data/DB.csv', index=False)
+
